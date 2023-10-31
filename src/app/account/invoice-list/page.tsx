acb93c31b79c7422fc69cf84a048e36981906937
SuzuyaBish <sloan.jarrod@gmail.com>
2023-10-31 14:47:26 +0200

    new routes + dashboard

---
+++ src/app/account/invoice-list/page.tsx
@@ -0,0 +1,11 @@
+import InvoiceTable from "@/components/InvoiceTable"
+import InvoiceListHeading from "@/components/layout/InvoiceListHeading"
+
+export default function InvoiceList() {
+  return (
+    <div>
+      <InvoiceListHeading />
+      <InvoiceTable />
+    </div>
+  )
+}
