63fe74d6a1aa8c464991c68beba0342ab44b3470
SuzuyaBish <sloan.jarrod@gmail.com>
2023-10-31 15:00:15 +0200

    remove gradient from dashboard

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Fragment, useState } from "react"
+import { Fragment } from "react"
 import { classNames } from "@/constants/tailwind-constants"
 import { Menu, Transition } from "@headlessui/react"
 import {
@@ -11,12 +11,6 @@ import {
   PlusSmallIcon,
 } from "@heroicons/react/20/solid"
 
-const navigation = [
-  { name: "Home", href: "#" },
-  { name: "Invoices", href: "#" },
-  { name: "Clients", href: "#" },
-  { name: "Expenses", href: "#" },
-]
 const secondaryNavigation = [
   { name: "Last 7 days", href: "#", current: true },
   { name: "Last 30 days", href: "#", current: false },
@@ -147,7 +141,6 @@ const clients = [
 ]
 
 export default function Account() {
-
   return (
     <main>
       <div className="relative isolate overflow-hidden ">
@@ -212,19 +205,6 @@ export default function Account() {
               </div>
             ))}
           </dl>
-        </div>
-
-        <div
-          className="absolute left-0 top-full -z-10 mt-96 origin-top-left translate-y-40 -rotate-90 transform-gpu opacity-20 blur-3xl sm:left-1/2 sm:-ml-96 sm:-mt-10 sm:translate-y-0 sm:rotate-0 sm:transform-gpu sm:opacity-50"
-          aria-hidden="true"
-        >
-          <div
-            className="aspect-[1154/678] w-[72.125rem] bg-gradient-to-br from-[#FF80B5] to-[#9089FC]"
-            style={{
-              clipPath:
-                "polygon(100% 38.5%, 82.6% 100%, 60.2% 37.7%, 52.4% 32.1%, 47.5% 41.8%, 45.2% 65.6%, 27.5% 23.4%, 0.1% 35.3%, 17.9% 0%, 27.7% 23.4%, 76.2% 2.5%, 74.2% 56%, 100% 38.5%)",
-            }}
-          />
         </div>
       </div>
 
