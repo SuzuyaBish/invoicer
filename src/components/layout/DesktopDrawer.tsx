874c692e5d745eb8a88cfc2e9c892637da2b6e8e
SuzuyaBish <sloan.jarrod@gmail.com>
2023-11-01 10:06:06 +0200

    crude settings screen added

--- src/components/layout/DesktopDrawer.tsx
+++ src/components/layout/DesktopDrawer.tsx
@@ -69,7 +69,7 @@ export default function DesktopDrawer() {
             </li>
             <li className="mt-auto">
               <a
-                href="#"
+                href="/account/settings"
                 className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
               >
                 <Cog6ToothIcon
--- src/components/layout/MobileDrawer.tsx
+++ src/components/layout/MobileDrawer.tsx
@@ -126,7 +126,7 @@ export function MobileDrawer() {
                     </li>
                     <li className="mt-auto">
                       <a
-                        href="#"
+                        href="/account/settings"
                         className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
                       >
                         <Cog6ToothIcon
