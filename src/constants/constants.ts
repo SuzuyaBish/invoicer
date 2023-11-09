7dc9a918cdecda43c7f623ad7cbd8484e3c12911
SuzuyaBish <sloan.jarrod@gmail.com>
2023-11-09 22:33:18 +0200

    Update status chip colors for dark mode

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -37,9 +37,9 @@ export const clients: Client[] = [
 ]
 
 export const statuses = {
-  Paid: "text-green-700 bg-green-50 ring-green-600/20",
-  Withdraw: "text-gray-600 bg-gray-50 ring-gray-500/10",
-  Overdue: "text-red-700 bg-red-50 ring-red-600/10",
+  Paid: "text-green-700 bg-green-50 ring-green-600/20 dark:bg-green-900/20 dark:ring-emerald-600/10 dark:text-emerald-200",
+  Withdraw: "text-gray-600 bg-gray-50 ring-gray-500/10 dark:bg-gray-900/20 dark:ring-gray-500/10 dark:text-gray-200",
+  Overdue: "text-red-700 bg-red-50 ring-red-600/10 dark:bg-red-900/20 dark:ring-red-600/10 dark:text-red-200",
 }
 
 export const invoice: InvoiceTable = {
