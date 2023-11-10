14e4b5c1cc0f21e256563807acb6ec331fc45af7
SuzuyaBish <sloan.jarrod@gmail.com>
2023-11-10 13:13:26 +0200

    invoice table empty state added

--- src/components/InvoiceTableItems.tsx
+++ src/components/InvoiceTableItems.tsx
@@ -25,97 +25,123 @@ const InvoiceTableItems: FC<InvoiceTableItemsProps> = ({
   return (
     <tbody>
       <AnimatePresence>
-        {items
-          .filter((item, index) => {
-            if (searchParams.tab === "all") {
-              return item
-            } else {
-              return item.status === searchParams.tab
-            }
-          })
-          .map((item, itemIdx) => (
-            <motion.tr
-              key={item.id}
-              variants={{
-                hidden: { opacity: 0, y: -20 },
-                visible: (idx) => ({
-                  opacity: 1,
-                  y: 0,
-                  transition: {
-                    delay: idx * 0.05,
-                  },
-                }),
-              }}
-              initial="hidden"
-              animate="visible"
-              custom={itemIdx}
-            >
-              <td
-                className={classNames(
-                  itemIdx !== items.length - 1 ? "border-b" : "",
-                  "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-foreground sm:pl-6 lg:pl-8"
-                )}
-              >
-                INV# {item.id}
-              </td>
-              <td
-                className={classNames(
-                  itemIdx !== items.length - 1 ? "border-b" : "",
-                  "whitespace-nowrap hidden px-3 py-4 text-sm text-foreground sm:table-cell"
-                )}
-              >
-                {item.client.first_name}
-              </td>
-              <td
-                className={classNames(
-                  itemIdx !== items.length - 1 ? "border-b" : "",
-                  "whitespace-nowrap hidden px-3 py-4 text-sm text-foreground lg:table-cell"
-                )}
-              >
-                {item.information.invoicedDate !== "" ? (
-                  <>{format(new Date(item.information.invoicedDate), "PP")}</>
-                ) : (
-                  <div>N/A</div>
-                )}
-              </td>
-              <td
-                className={classNames(
-                  itemIdx !== items.length - 1 ? "border-b" : "",
-                  "whitespace-nowrap px-3 py-4 text-sm text-foreground capitalize"
-                )}
-              >
-                {item.status}
-              </td>
-              <td
-                className={classNames(
-                  itemIdx !== items.length - 1 ? "border-b" : "",
-                  "whitespace-nowrap px-3 py-4 text-sm text-foreground"
-                )}
-              >
-                {item.information.currency}{" "}
-                {calculateTotal(
+        {items.filter((item) => {
+          if (searchParams.tab === "all") {
+            return item
+          } else {
+            return item.status === searchParams.tab
+          }
+        }).length > 0 ? (
+          items
+            .filter((item) => {
+              if (searchParams.tab === "all") {
+                return item
+              } else {
+                return item.status === searchParams.tab
+              }
+            })
+            .map((item, itemIdx) => {
+              const total = Number(
+                calculateTotal(
                   calculateSubTotal(item.table.items),
                   calculateTax(
                     calculateSubTotal(item.table.items),
                     Number(item.table.tax)
                   )
-                )}
-              </td>
-              <td
-                className={classNames(
-                  itemIdx !== items.length - 1 ? "border-b" : "",
-                  "relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8"
-                )}
-              >
-                <Link
-                  href={`/account/invoice-list/preview/${item.id}`}
-                  className="text-muted-foreground bg-muted hover:text-foreground rounded p-2"
+                )
+              ).toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })
+              return (
+                <motion.tr
+                  key={item.id}
+                  variants={{
+                    hidden: { opacity: 0, y: -20 },
+                    visible: (idx) => ({
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        delay: idx * 0.05,
+                      },
+                    }),
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  custom={itemIdx}
                 >
-                  Preview<span className="sr-only">, {item.id}</span>
-                </Link>
-              </td>
-            </motion.tr>
-          ))}
+                  <td
+                    className={classNames(
+                      itemIdx !== items.length - 1 ? "border-b" : "",
+                      "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-foreground sm:pl-6 lg:pl-8"
+                    )}
+                  >
+                    INV# {item.id}
+                  </td>
+                  <td
+                    className={classNames(
+                      itemIdx !== items.length - 1 ? "border-b" : "",
+                      "whitespace-nowrap hidden px-3 py-4 text-sm text-foreground sm:table-cell"
+                    )}
+                  >
+                    {item.client.first_name}
+                  </td>
+                  <td
+                    className={classNames(
+                      itemIdx !== items.length - 1 ? "border-b" : "",
+                      "whitespace-nowrap hidden px-3 py-4 text-sm text-foreground lg:table-cell"
+                    )}
+                  >
+                    {item.information.invoicedDate !== "" ? (
+                      <>
+                        {format(new Date(item.information.invoicedDate), "PP")}
+                      </>
+                    ) : (
+                      <div>N/A</div>
+                    )}
+                  </td>
+                  <td
+                    className={classNames(
+                      itemIdx !== items.length - 1 ? "border-b" : "",
+                      "whitespace-nowrap px-3 py-4 text-sm text-foreground capitalize"
+                    )}
+                  >
+                    {item.status}
+                  </td>
+                  <td
+                    className={classNames(
+                      itemIdx !== items.length - 1 ? "border-b" : "",
+                      "whitespace-nowrap px-3 py-4 text-sm text-foreground"
+                    )}
+                  >
+                    {item.information.currency} {total}
+                  </td>
+                  <td
+                    className={classNames(
+                      itemIdx !== items.length - 1 ? "border-b" : "",
+                      "relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8"
+                    )}
+                  >
+                    <Link
+                      href={`/account/invoice-list/preview/${item.id}`}
+                      className="text-muted-foreground bg-muted hover:text-foreground rounded p-2"
+                    >
+                      Preview<span className="sr-only">, {item.id}</span>
+                    </Link>
+                  </td>
+                </motion.tr>
+              )
+            })
+        ) : (
+          <motion.tr initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <td
+              colSpan={5}
+              className="text-muted-foreground whitespace-nowrap px-6 py-4 text-center text-sm font-medium capitalize"
+            >
+              No invoices
+            </td>
+          </motion.tr>
+        )}
       </AnimatePresence>
     </tbody>
   )
